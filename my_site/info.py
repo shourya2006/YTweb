b3f211a6b5ba49aaecf0849ca50d64ac8654350d
Cyber Programmer <75479705+shourya2006@users.noreply.github.com>
2024-10-08 23:56:45 +0530

    Main Commit

---
+++ my_site/info.py
@@ -0,0 +1,7 @@
+# Sign-Up Wont Work without these
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '' #Your Email
+EMAIL_HOST_PASSWORD = '' #You Host Pass after 2FA
+EMAIL_PORT = 587
